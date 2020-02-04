c824a79cbaec724d1b27ebe9c14409d35435dd1a
Michal Baran <baranmichal25@gmail.com>
2020-02-04 22:57:24 +0100

    Updated dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Provider<OrderBloc>(
-      builder: (context) => OrderBloc(),
+      create: (context) => OrderBloc(),
       dispose: (context, repo) => repo.dispose(),
       child: MaterialApp(
         title: 'Restaurant',
