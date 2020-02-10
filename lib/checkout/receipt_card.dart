6754c225c1575540f2583dcac5f18d207241905d
Michal Baran <baranmichal25@gmail.com>
2020-02-10 22:24:06 +0100

    Receipt is now using Consumer<OrderChangeNotifier>

--- lib/checkout/receipt_card.dart
+++ lib/checkout/receipt_card.dart
@@ -1,20 +1,21 @@
-import 'package:everything_provider/change_notifier/order.dart';
+import 'package:everything_provider/change_notifier/order_change_notifier.dart';
 import 'package:everything_provider/commons/formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Receipt extends StatelessWidget {
-  final Order order;
-
-  const Receipt(this.order);
-
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(
-          Formatter.getReceiptFrom(order),
-          style: TextStyle(fontSize: 18.0),
+        child: Consumer<OrderChangeNotifier>(
+          builder: (context, notifier, _) {
+            return Text(
+              Formatter.getReceiptFrom(notifier.order),
+              style: TextStyle(fontSize: 18.0),
+            );
+          },
         ),
       ),
     );
